2b3b77da133a4b2e2a6540fcfc0393a8ec829292
bardhansubhajit3@gmail.com <bardhansubhajit3@gmail.com>
2025-03-15 03:17:30 +0530

    Working in Java Advanced

---
+++ Java advanced/Test.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+class students<k, v> {
+    private Map<k, v> studentsData = new HashMap<>();
+
+    public void set(k studentName, v marks) {
+        studentsData.put(studentName, marks);
+    }
+
+    public v get(k studentName) {
+        return studentsData.get(studentName);
+    }
+}
+
+public class Test {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        students<String, Integer> StudentMarks = new students<>();
+        StudentMarks.set("Subhajit", 100);
+        StudentMarks.set("Sohini", 98);
+        System.out.println(StudentMarks.get("Subhajit"));
+        System.out.println(StudentMarks.get("Sohini"));
+    }
+}
---
+++ Java advanced/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+for (int i = 0; i < arraylist1.length; i++) {
+        // System.out.println(arraylist1[i]);
+        // }
